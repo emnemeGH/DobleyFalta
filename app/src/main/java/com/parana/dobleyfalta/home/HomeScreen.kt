e97b29ee2936db80843c07ef5176601dbd1ef743
Nao <nao.kakisu@gmail.com>
2025-09-01 05:48:47 -0300

    creación de home.kt

---
+++ app/src/main/java/com/parana/dobleyfalta/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.parana.dobleyfalta.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun InicioScreen(navController: NavController){
+
+}
