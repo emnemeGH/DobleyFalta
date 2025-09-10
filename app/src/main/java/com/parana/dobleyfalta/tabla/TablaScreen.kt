5cc9ceda55ab5f27765db66badefd50a79e7c65c
Nao <nao.kakisu@gmail.com>
2025-09-10 07:12:56 -0300

    Update TablaScreen: Cambios de estilo y formado, tabla con scroll

--- app/src/main/java/com/parana/dobleyfalta/tabla/TablaScreen.kt
+++ app/src/main/java/com/parana/dobleyfalta/tabla/TablaScreen.kt
@@ -1,29 +1,36 @@
 package com.parana.dobleyfalta.tabla
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import com.parana.dobleyfalta.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.parana.dobleyfalta.DarkBlue
+import com.parana.dobleyfalta.DarkGrey
+import com.parana.dobleyfalta.noticias.LightGrey
 
 data class EquipoTabla(
     val id: Int,
@@ -38,68 +45,109 @@ data class EquipoTabla(
     val pc: Int,
 )
 
+val equiposTabla = listOf(
+    EquipoTabla(1, 1, "Paracao", R.drawable.escudo_paracao, 53, 38, 15, 23, 3018, 3125),
+    EquipoTabla(2, 2, "Rowing", R.drawable.escudo_rowing, 52, 38, 14, 24, 3237, 3348),
+    EquipoTabla(3, 3, "CAE", R.drawable.escudo_cae, 51, 38, 13, 25, 3199, 3286),
+    EquipoTabla(4, 4, "Ciclista", R.drawable.escudo_rowing, 48,38, 10, 28, 2723, 3099),
+    EquipoTabla(5, 5, "Quique", R.drawable.escudo_cae, 44, 28, 6, 32, 2724, 3216)
+)
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTablaScreen(){
+    TablaScreen()
+}
+
 @Composable
 fun TablaScreen (){
-    val equiposTabla = listOf(
-        EquipoTabla(1, 1, "Paracao", R.drawable.escudo_paracao, 53, 38, 15, 23, 3018, 3125),
-        EquipoTabla(2, 2, "Rowing", R.drawable.escudo_rowing, 52, 38, 14, 24, 3237, 3348),
-        EquipoTabla(3, 3, "CAE", R.drawable.escudo_cae, 51, 38, 13, 25, 3199, 3286),
-        EquipoTabla(4, 4, "Ciclista", R.drawable.escudo_rowing, 48,38, 10, 28, 2723, 3099),
-        EquipoTabla(5, 5, "Quique", R.drawable.escudo_cae, 44, 28, 6, 32, 2724, 3216)
-    )
-
-    var listaEquiposTabla by remember { mutableStateOf(equiposTabla) }
-    val listaEquiposTablaOrdenada = remember(listaEquiposTabla) {
-        listaEquiposTabla.sortedBy { it.nombre }
-    }
-
+    val DarkBlue = colorResource(id = R.color.darkBlue)
+    val PrimaryOrange = colorResource(id = R.color.primaryOrange)
+    val White = colorResource(id = R.color.white)
+    val DarkGrey = Color(0xFF1A375E)
+    val LightGrey = Color(0xFFA0B3C4)
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(DarkBlue)
             .padding(16.dp)
     ) {
+
         Text(
-            text = "TABLA DE POSICIONES TORNEO 2024/25",
+            text = "TABLA DE POSICIONES TORNEO",
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
-            modifier = Modifier.padding(bottom = 16.dp)
+            color = White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp, bottom = 10.dp),
+            textAlign = TextAlign.Center
+        )
+
+        Text(
+            text = "Desliza la tabla para ver más información",
+            fontSize = 14.sp,
+            color = White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            textAlign = TextAlign.Center
         )
 
         // Encabezado de tabla
-        Row(
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.Black)
-                .padding(vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            TablaHeader("N°", 0.1f)
-            TablaHeader("Nombre", 0.3f)
-            TablaHeader("Puntos", 0.1f)
-            TablaHeader("P.J", 0.1f)
-            TablaHeader("P.G", 0.1f)
-            TablaHeader("P.P", 0.1f)
+                .fillMaxSize()
+                .horizontalScroll(rememberScrollState())
+        ){
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(1.dp, Color.Black),
+                colors = CardDefaults.cardColors(containerColor = Color.White)
+            ) {
+                // Lista de equipos
+                Column() {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(DarkGrey)
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        TablaHeader("N°", Modifier.width(50.dp))
+                        TablaHeader("Equipo", Modifier.width(150.dp))
+                        TablaHeader("Puntos", Modifier.width(65.dp))
+                        TablaHeader("P.J", Modifier.width(50.dp))
+                        TablaHeader("P.G", Modifier.width(50.dp))
+                        TablaHeader("P.P", Modifier.width(60.dp))
+                        TablaHeader("P.F", Modifier.width(70.dp))
+                        TablaHeader("P.C", Modifier.width(55.dp))
+                    }
+
+                    LazyColumn {
+                        items(equiposTabla) { equipo ->
+                            EquipoFila(equipo)
+                        }
+                    }
+                }
+            }
+
         }
 
-        // Lista de equipos
-        LazyColumn {
-            items(equiposTabla) { equipo ->
-                EquipoFila(equipo)
-            }
-        }
     }
 }
 
 @Composable
-fun TablaHeader(text: String, weight: Float) {
+fun TablaHeader(text: String, modifier: Modifier) {
     Text(
         text = text,
         color = Color.White,
         fontWeight = FontWeight.Bold,
-        //modifier = Modifier.weight(weight),                         //Ver por qué no funciona y cómo arreglarlo
-        fontSize = 12.sp
+        modifier = modifier.padding(4.dp) .fillMaxWidth(),
+        fontSize = 14.sp
     )
 }
 
@@ -108,23 +156,39 @@ fun EquipoFila(equipo: EquipoTabla) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color(0xFFF7F7F7))
+            .background(LightGrey)
             .padding(vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(equipo.posicion.toString(), modifier = Modifier.weight(0.1f))
-        Row(modifier = Modifier.weight(0.3f), verticalAlignment = Alignment.CenterVertically) {
+        CeldaEquipo(equipo.posicion.toString(), Modifier.width(56.dp) .padding(start = 10.dp))
+        Row(modifier = Modifier.width(160.dp), verticalAlignment = Alignment.CenterVertically) {
             Image(
                 painter = painterResource(id = equipo.escudoUrl),
                 contentDescription = equipo.nombre,
                 modifier = Modifier.size(24.dp)
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Text(equipo.nombre, fontWeight = FontWeight.SemiBold, fontSize = 12.sp)
+            Text(equipo.nombre, fontWeight = FontWeight.SemiBold, fontSize = 14.sp)
         }
-        Text(equipo.puntos.toString(), modifier = Modifier.weight(0.1f))
-        Text(equipo.pj.toString(), modifier = Modifier.weight(0.1f))
-        Text(equipo.pg.toString(), modifier = Modifier.weight(0.1f))
-        Text(equipo.pp.toString(), modifier = Modifier.weight(0.1f))
+        CeldaEquipo(equipo.puntos.toString(), Modifier.width(60.dp))
+        CeldaEquipo(equipo.pj.toString(), Modifier.width(50.dp))
+        CeldaEquipo(equipo.pg.toString(), Modifier.width(50.dp))
+        CeldaEquipo(equipo.pp.toString(), Modifier.width(50.dp))
+        CeldaEquipo(equipo.pf.toString(), Modifier.width(70.dp))
+        CeldaEquipo(equipo.pc.toString(), Modifier.width(70.dp))
+
     }
+    HorizontalDivider(
+        thickness = 1.dp
+    )
+}
+
+@Composable
+fun CeldaEquipo(texto: String, modifier: Modifier) {
+    Text(
+        text = texto,
+        fontSize = 14.sp,
+        modifier = modifier.padding(4.dp),
+        color = Color.Black
+    )
 }
