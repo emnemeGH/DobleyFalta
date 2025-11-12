cc2f5ce4520ebabdd92026c06962ef7cfe9d86f8
emnemeGH <emaneme05@gmail.com>
2025-11-12 20:43:53 -0300

    Token a usuarios y notcias

--- app/src/main/java/com/parana/dobleyfalta/retrofit/clients/RetrofitClientUsuarios.kt
+++ app/src/main/java/com/parana/dobleyfalta/retrofit/clients/RetrofitClientUsuarios.kt
@@ -1,16 +1,33 @@
 package com.parana.dobleyfalta.retrofit.clients
 
+import android.content.Context
+import com.parana.dobleyfalta.SessionManager
 import com.parana.dobleyfalta.retrofit.ApiConstants
+import com.parana.dobleyfalta.retrofit.interceptors.AuthInterceptor
 import com.parana.dobleyfalta.retrofit.services.UsuariosApiService
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClientUsuarios {
-    val usuariosApiService: UsuariosApiService by lazy {
-        Retrofit.Builder()
+    private var retrofit: Retrofit? = null
+
+    fun init(context: Context) {
+        val sessionManager = SessionManager(context.applicationContext)
+        val authInterceptor = AuthInterceptor(sessionManager)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+
+        retrofit = Retrofit.Builder()
             .baseUrl(ApiConstants.BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(UsuariosApiService::class.java)
     }
+
+    val usuariosApiService: UsuariosApiService
+        get() = retrofit?.create(UsuariosApiService::class.java)
+            ?: throw IllegalStateException("RetrofitClientUsuarios not initialized")
 }
---
+++ app/src/main/java/com/parana/dobleyfalta/retrofit/interceptors/AuthInterceptor.kt
@@ -0,0 +1,19 @@
+package com.parana.dobleyfalta.retrofit.interceptors
+
+import com.parana.dobleyfalta.SessionManager
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val sessionManager: SessionManager) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = sessionManager.getToken()
+
+        val requestBuilder = chain.request().newBuilder()
+
+        if (token != null) {
+            requestBuilder.addHeader("Authorization", "Bearer $token")
+        }
+
+        return chain.proceed(requestBuilder.build())
+    }
+}
